827ea886ea144709096a1c0032e0bec1db65d9f8
ESPGranEdu <raplhwigum@gmail.com>
2019-08-26 23:15:21 +0200

    Test

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+"use strict";
 //  Libraries
 
 const os = require("os");
 const color = require("colors");
 const Table = require("cli-table");
-const cli = require('commander');
-
+// const cli = require("commander");
 const table = new Table();
+
 //Implementation of format for JavaScript
+
 String.prototype.format = function() {
-  a = this;
-  for (k in arguments) {
+  let a = this;
+  for (let k in arguments) {
     a = a.replace("{" + k + "}", arguments[k]);
   }
   return a;
@@ -19,14 +21,23 @@ String.prototype.format = function() {
 function get_cpu() {
   let model = os.cpus()[0]["model"];
   let cores = os.cpus().length;
+
+  // Check if the processor it's from Intel or AMD
+  if (!model.search("Intel")) {
+    model = model.bold.blue;
+  } else if (!model.search("AMD")) {
+    model = model.bold.red;
+  } else {
+    model = model.bold.yellow;
+  }
   return "{0} ({1})".format(model, cores);
 }
 
 // Main Program
 
 table.push(
-  {'CPU': get_cpu()},
-  {'GPU': 'una Nvidia muy tocha'}
-);
+  { 'Hostname': os.hostname()},
+  { 'CPU': get_cpu() }, 
+  { 'GPU': "una Nvidia muy tocha" });
 
 console.log(table.toString());
