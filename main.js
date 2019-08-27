3932cfa74c282129f3de9d93d9a8495fe241b439
ESPGranEdu <raplhwigum@gmail.com>
2019-08-27 14:30:58 +0200

    Fixed get_gpu

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+// Set the Strict mode
 "use strict";
+
 //  Libraries
+
 const sys = require("systeminformation");
 const os = require("os");
 const color = require("colors");
 const Table = require("cli-table");
-// const cli = require("commander");
+const cli = require("commander");
+const command = require("child_process");
 const table = new Table();
 
-//Implementation of format for JavaScript
+// Implementation of format for JavaScript
 
 String.prototype.format = function() {
   let a = this;
@@ -17,13 +21,14 @@ String.prototype.format = function() {
   return a;
 };
 
-// Fetcher functions
+// Fetcher Functions
 
 function get_cpu() {
   let model = os.cpus()[0]["model"];
   let cores = os.cpus().length;
 
-  // Check if the processor it's from Intel or AMD
+  //? Check if the processor it's from Intel or AMD
+
   if (!model.search("Intel")) {
     model = model.bold.blue;
   } else if (!model.search("AMD")) {
@@ -35,46 +40,49 @@ function get_cpu() {
 }
 
 function get_mem() {
-  let freemem = Number.parseFloat(os.freemem() / 1024 ** 2).toFixed(0);
-  let totalmem =
+  let useMem = Number.parseFloat(
+    (os.totalmem() - os.freemem()) / 1024 ** 2
+  ).toFixed(0);
+
+  let totalMem =
     Number.parseFloat(os.totalmem() / 1024 ** 3)
       .toFixed(0)
       .toString() + " GB";
 
-  // If Free Memory is higher than 1024 MB, converto to GB with 1 decimal point
-  if (freemem >= 1024) {
-    freemem =
-      Number.parseFloat(freemem / 1024)
+  // If useMem is higher tan 1024 MB, converto to GB with 1 decimal point
+  if (useMem >= 1024) {
+    useMem =
+      Number.parseFloat(useMem / 1024)
         .toFixed(1)
         .toString() + " GB";
   } else {
-    freemem = freemem.toString() + " MB";
+    useMem = useMem.toString() + " MB";
   }
-  return "{0} / {1} ".format(freemem, totalmem);
+
+  return "{0} / {1}".format(useMem, totalMem);
 }
 
+//TODO Solve Promise issue with this function
 function get_gpu() {
-  const model = sys.graphics(function(data, err) {
-    if (err) {
-      console.log(err);
-    }
-    if (data) {
-      let gpu = data["controllers"][0]["model"];
-      console.log(parsed);
-    }
-  });
-  return model;
+  const info = command
+    .execSync('lspci | grep VGA | cut -d ":" -f3 | cut -d "(" -f1')
+    .toString()
+    .trim();
+
+  if (!info.search("NVIDIA")) return info.bold.green;
+  if (!info.search("AMD")) return info.bold.red;
+  if (!info.search("Intel")) return info.bold.blue;
+
+  return info.bold.orange;
 }
-// Main Program
 
-// table.push(
-//   { Hostname: os.hostname() },
-//   { CPU: get_cpu() },
-//   { GPU: "" },
-//   { Memory: get_mem() }
-// );
+// * Main Program
 
-// // Print the table
-// console.log(table.toString());
+table.push(
+  { Hostname: os.hostname() },
+  { CPU: get_cpu() },
+  { GPU: get_gpu() },
+  { Memory: get_mem() }
+);
 
-get_gpu();
+console.log(table.toString()); // Print the table
